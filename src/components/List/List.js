c7175bbb9adc3a5a0085c98d0da7863e78626098
leeyoonseo <okayoon.lee@gmail.com>
2018-09-17 18:59:26 +0900

    newlist push하던 부분 filter함수로 해결

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,40 +1,29 @@
 import React, {Component} from 'react';
 import './List.css';
 
-const getNewList = (newList,list, keyWord) => {
-    list.findIndex(items => {
-        const item = items.toLowerCase(); 
-        const removeSpaceItem = item.replace( /(\s*)/g, "");
-        const removeSpaceKeyWord = keyWord.replace( /(\s*)/g, "");
-
-        if(removeSpaceItem.indexOf(removeSpaceKeyWord.toLowerCase()) == 0){
-            newList.push(item);
-        }
-    });
-}
-
 class List extends Component{
     shouldComponentUpdate(nextProps, nextState){
-        if(this.props.keyWord !== nextProps.keyWord){
-            return true;
-        }
-        return false;
+        return this.props.keyWord !== nextProps.keyWord;
     }
 
     render(){
         const {list, keyWord} = this.props;
-        const newList = [];
-        getNewList(newList, list, keyWord);
+        const removeSpace = (item) => {
+            return item.replace( /(\s*)/g, "");
+        };
+        const removeSpaceKeyWord = removeSpace(keyWord);
 
         return(
             <ul className="list">
                 {
-                    keyWord !== '' 
-                        ?   newList.map((val,index) => {
-                                return(<li key={index}>{val}</li>)
-                            })
-                        :   []
-                         
+                    keyWord !== ''
+                    ?   list.filter(item => {
+                            item = item.toLowerCase(); 
+                            if(removeSpace(item).indexOf(removeSpaceKeyWord.toLowerCase()) === 0){
+                                return item;
+                            }
+                        })
+                    : []
                 }
             </ul>
         );
