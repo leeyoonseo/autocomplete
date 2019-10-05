0fdbe014d7dbb13402444fffb64ffb9fdc512fdd
leeyoonseo <okayoon.lee@gmail.com>
2019-10-05 17:01:10 +0900

    예제추가

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     keyWord : '',
     list : [
       '가나다라마',
+      '가나다라',
       '나다라마바',
       '다라마바사',
       '사이렌',
@@ -49,10 +50,17 @@ class App extends Component {
     const {list, keyWord} = this.state;
     
     return (
-      <div className="app">
+      <div className="app">        
         <Input onChange={this.handleChange}/>
         <Button />
         <List list={list} keyWord={keyWord}/>
+        <br/>
+        <br/>
+        <div>
+          테스트 텍스트 목록<br/>
+          가나다라마, 가나다라, 나다라마바, 다라마바사, 사이렌, 모든 날, 모든 순간, 북극성, 뚜두뚜두, 여보세요, 열애중, 여행, 너나 해, 지나오다, 한, 셋 셀테니, 몰랐니, 데리러 가, 너만 괜찮다면
+          , 밤편지, 전하지 못한 진심, 뿜뿜, 브람스 자장가, abcdef, ABC마트
+        </div>
       </div>
     );
   }
